ef371135a298b410c6c7dbc798418b66f3e6814c
eyo-as <eyosiyastumiso7@gmail.com>
2024-07-11 21:00:57 +0300

    update deploy

--- netflix-clone/vite.config.js
+++ netflix-clone/vite.config.js
@@ -5,4 +5,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   base: "/Netflix-Clone/netflix-clone",
   plugins: [react()],
+  define: {
+    "process.env": process.env,
+  },
 });
